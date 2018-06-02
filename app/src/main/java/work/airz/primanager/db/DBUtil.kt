192c45c09fe59f589e9dbc5a3d2e314a6d40266d
khrom <devs@outlook.jp>
2018-06-02 23:17:58 +0900

    add 追加部分を書いた fix 色々な誤字を直した。

--- app/src/main/java/work/airz/primanager/db/DBUtil.kt
+++ app/src/main/java/work/airz/primanager/db/DBUtil.kt
@@ -2,6 +2,7 @@ package work.airz.primanager.db
 
 import android.content.Context
 import android.graphics.Bitmap
+import org.jetbrains.anko.db.replace
 import org.jetbrains.anko.db.parseList
 import org.jetbrains.anko.db.rowParser
 import org.jetbrains.anko.db.select
@@ -14,38 +15,78 @@ class DBUtil(private val context: Context) {
      * フォローデータのリスト取得用
      * @return フォローチケットのリスト
      */
-    fun getFollowList(): List<FollowTicket> {
+    fun getFollowTicketList(): List<FollowTicket> {
         return database.use {
             select(DBConstants.FOLLOW_TICKET_TABLE).exec {
-                parseList(rowParser { raw: String, user_id: String, name: String, date: String, follow: Int, follower: Int, coordinate: String, arcade_series: String, which_account: String, image: Bitmap, memo: String ->
-                    FollowTicket(raw, user_id, name, date, follow, follower, coordinate, arcade_series, which_account, image, memo)
+                parseList(rowParser { raw: String, userId: String, userName: String, date: String, follow: Int, follower: Int, coordinate: String, arcade_series: String, whichAccount: String, image: Bitmap, memo: String ->
+                    FollowTicket(raw, userId, userName, date, follow, follower, coordinate, arcade_series, whichAccount, image, memo)
                 })
             }
         }
     }
 
     /**
+     * フォロチケデータ追加
+     */
+    fun putFollowTicketData(followTicket: FollowTicket) {
+        database.use {
+            replace(DBConstants.FOLLOW_TICKET_TABLE,
+                    DBConstants.RAW to followTicket.raw,
+                    DBConstants.USER_ID to followTicket.userId,
+                    DBConstants.USER_NAME to followTicket.userName,
+                    DBConstants.DATE to followTicket.date,
+                    DBConstants.FOLLOW to followTicket.follow,
+                    DBConstants.FOLLOWER to followTicket.follower,
+                    DBConstants.COORDINATE to followTicket.coordinate,
+                    DBConstants.ARCADE_SERIES to followTicket.arcade_series,
+                    DBConstants.IMAGE to followTicket.image,
+                    DBConstants.MEMO to followTicket.memo)
+        }
+    }
+
+
+
+    /**
      * コーデチケットのリスト取得用
      * @return コーデチケットのリスト
      */
-    fun getCoordList(): List<CoordTicket> {
+    fun getCoordTicketList(): List<CoordTicket> {
         return database.use {
             select(DBConstants.COORD_TICKET_TABLE).exec {
-                parseList(rowParser { raw: String, codeID: String, codeName: String, rarity: String, brand: String, color: String, arcadeSeries: String, date: String, whichAccount: String, image: Bitmap, memo: String ->
-                    CoordTicket(raw, codeID, codeName, rarity, brand, color, arcadeSeries, date, whichAccount, image, memo)
+                parseList(rowParser { raw: String, coordId: String, coordName: String, rarity: String, brand: String, color: String, arcadeSeries: String, date: String, whichAccount: String, image: Bitmap, memo: String ->
+                    CoordTicket(raw, coordId, coordName, rarity, brand, color, arcadeSeries, date, whichAccount, image, memo)
                 })
             }
         }
     }
 
+    /**
+     * コーデチケット追加
+     */
+    fun putCoordTicketData(coodTicket: CoordTicket){
+        database.use {
+            replace(DBConstants.COORD_TICKET_TABLE,
+                    DBConstants.RAW to coodTicket.raw,
+                    DBConstants.COORD_ID to coodTicket.coordId,
+                    DBConstants.COORD_NAME to coodTicket.coordName,
+                    DBConstants.RARITY to coodTicket.rarity,
+                    DBConstants.BRAND to coodTicket.brand,
+                    DBConstants.COLOR to coodTicket.color,
+                    DBConstants.ARCADE_SERIES to coodTicket.arcadeSeries,
+                    DBConstants.DATE to coodTicket.date,
+                    DBConstants.WHICH_ACCOUNT to coodTicket.whichAccount,
+                    DBConstants.IMAGE to coodTicket.image,
+                    DBConstants.MEMO to coodTicket.memo)
+        }
+    }
     /**
      * ユーザ一覧を返す
      */
     fun getUsers(): List<User> {
         return database.use {
             select(DBConstants.USER_TABLE).exec {
-                parseList(rowParser { raw: String, user_name: String, user_card_id: String, follows: String ->
-                    User(raw, user_name, user_card_id, follows)
+                parseList(rowParser { raw: String, userName: String, userCardId: String, follows: String ->
+                    User(raw, userName, userCardId, follows)
                 })
             }
         }
@@ -76,13 +117,11 @@ class DBUtil(private val context: Context) {
         }
     }
 
-    
-
 
     class CoordTicket(
             val raw: String,
-            val codeID: String,
-            val codeName: String,
+            val coordId: String,
+            val coordName: String,
             val rarity: String,
             val brand: String,
             val color: String,
@@ -94,20 +133,20 @@ class DBUtil(private val context: Context) {
 
     class FollowTicket(
             val raw: String,
-            val user_id: String,
-            val name: String,
+            val userId: String,
+            val userName: String,
             val date: String,
             val follow: Int,
             val follower: Int,
             val coordinate: String,
             val arcade_series: String,
-            val which_account: String,
+            val whichAccount: String,
             val image: Bitmap,
             val memo: String)
 
     class User(
             val raw: String,
-            val user_name: String,
-            val user_card_id: String,
+            val userName: String,
+            val userCardId: String,
             val follows: String)
 }
