5315b7df056505cf2b8da263ee6b50ea261ea771
khrom <devs@outlook.jp>
2018-06-06 18:07:39 +0900

    add 保存部分のactivityを追加

---
+++ app/src/main/java/work/airz/primanager/SaveFollowTicket.kt
@@ -0,0 +1,12 @@
+package work.airz.primanager
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SaveFollowTicket : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save_follow_ticket)
+    }
+}
