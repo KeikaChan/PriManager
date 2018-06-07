97373621fb85737e3ef23151ec3e37f3f333ab53
khrom <devs@outlook.jp>
2018-06-07 22:22:20 +0900

    add 保存のボタンのリスナー周りの設定

--- app/src/main/java/work/airz/primanager/SaveCoordTicket.kt
+++ app/src/main/java/work/airz/primanager/SaveCoordTicket.kt
@@ -1,12 +1,46 @@
 package work.airz.primanager
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_save_coord_ticket.*
 
-class SaveCoordTicket : AppCompatActivity() {
+class SaveCoordTicket : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_save_coord_ticket)
+        save.setOnClickListener(this)
+        destruction.setOnClickListener(this)
+        continuation.setOnClickListener(this)
+        get_data.setOnClickListener(this)
+        display_qr.setOnClickListener(this)
+
     }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.save -> {
+                saveData()
+                finish()
+            }
+            R.id.destruction -> {
+                finish()
+            }
+            R.id.continuation -> {
+                saveData()
+                startActivity(Intent(this, QRActivity::class.java).apply { flags = Intent.FLAG_ACTIVITY_SINGLE_TOP })
+                finish()
+            }
+            R.id.get_data ->{
+
+            }
+        }
+    }
+
+    fun saveData() {
+
+    }
+
 }
--- app/src/main/java/work/airz/primanager/SaveFollowTicket.kt
+++ app/src/main/java/work/airz/primanager/SaveFollowTicket.kt
@@ -1,12 +1,48 @@
 package work.airz.primanager
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_save_follow_ticket.*
 
-class SaveFollowTicket : AppCompatActivity() {
+class SaveFollowTicket : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_save_follow_ticket)
+        save.setOnClickListener(this)
+        destruction.setOnClickListener(this)
+        continuation.setOnClickListener(this)
+        display_qr.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.save -> {
+                saveData()
+                finish()
+            }
+            R.id.destruction -> {
+                finish()
+            }
+            R.id.continuation -> {
+                saveData()
+                startActivity(Intent(this, QRActivity::class.java).apply { flags = Intent.FLAG_ACTIVITY_SINGLE_TOP })
+                finish()
+            }
+        }
+    }
+
+    /**
+     * 分かっているデータが合ったら
+     */
+    fun setData() {
+
+    }
+
+    fun saveData() {
+
     }
 }
