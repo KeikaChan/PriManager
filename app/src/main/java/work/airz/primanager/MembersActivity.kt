92fc8ea7f6d33ba1652b1ef5260f107de023d132
khrom <devs@outlook.jp>
2018-06-12 00:39:57 +0900

    add 詳細画面へのintentを入れた

--- app/src/main/java/work/airz/primanager/MembersActivity.kt
+++ app/src/main/java/work/airz/primanager/MembersActivity.kt
@@ -1,5 +1,6 @@
 package work.airz.primanager
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -9,6 +10,7 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_members.*
 import kotlinx.android.synthetic.main.ticket_item.view.*
 import work.airz.primanager.db.DBUtil
+import work.airz.primanager.qr.QRUtil
 
 class MembersActivity : AppCompatActivity(), IItemsList {
     private lateinit var dbUtil: DBUtil
@@ -38,12 +40,17 @@ class MembersActivity : AppCompatActivity(), IItemsList {
 
     override fun onItemClick(view: View, position: Int) {
         Toast.makeText(applicationContext, "position $position was tapped", Toast.LENGTH_SHORT).show()
+        startActivity(Intent(this, SaveUserTicket::class.java).apply {
+            putExtra(QRUtil.RAW, QRUtil.stringToByte(view.raw_data.text.toString()))
+            putExtra(QRUtil.IS_DUPLICATE, true)
+            flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+        })
     }
 
     override fun onItemList(): List<TicketUtils.TicketItemFormat> {
         val userList = mutableListOf<TicketUtils.TicketItemFormat>()
         dbUtil.getUserList().forEach {
-            userList.add(TicketUtils.TicketItemFormat(it.userName, it.userCardId, it.image))
+            userList.add(TicketUtils.TicketItemFormat(it.userName, it.userCardId, it.image, it.raw))
         }
         return userList.toList()
     }
